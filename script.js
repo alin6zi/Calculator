948c74905a73a952b8d814d1ccded03891f7ffd3
AliNazi <125884879+alin6zi@users.noreply.github.com>
2023-12-10 02:10:15 +0330

    update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const evalLine = document.getElementById('print');
 const numberButtons = document.querySelectorAll('.butt-num .number
 ');
 const doubleZero = document.getElementById('btn10')
-const doubleZero = document.getElementById('btn11')
+const doubleZero = document.getElementById('btn0')
 const pointBtn = document.getElementById('btn11');
 const addBtn = document.getElementById('opadd');
 const subBtn = document.getElementById('opsub');

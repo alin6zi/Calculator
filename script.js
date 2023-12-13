71aecb40caae91a716d8ad82ddc2b007816c416d
AliNazi <125884879+alin6zi@users.noreply.github.com>
2023-12-13 22:32:10 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const displayLine = document.getElementById('display');
 const evalLine = document.getElementById('print');
 const numberButtons = document.querySelectorAll('.butt-num .number');
-const doubleZero = document.getElementById('btn10');
 const numberZero = document.getElementById('btn0');
+const doubleZero = document.getElementById('btn10')
 const pointBtn = document.getElementById('btn11');
 const addBtn = document.getElementById('opadd');
 const subBtn = document.getElementById('opsub');
@@ -11,97 +11,223 @@ const mulBtn = document.getElementById('opmul');
 const evBtn = document.getElementById('opev');
 const clear = document.getElementById('clear');
 
-
 let opsBtn = [addBtn, subBtn, divBtn, mulBtn];
-let opsObj = "+-/*"
+
+const addOps = opsBtn[0].value;
+const subOps = opsBtn[1].value;
+const mulOps = opsBtn[2].value;
+const divOps = opsBtn[3].value;
 
 
 
 let currentExpression = "";
 let currentDisplay = "";
-let fuck = (currentExpression) * 1;
+
+
+let k;
+
+let memory;
+
+let opP;
+let opS;
+let opD;
+let opM;
+let lastIndex;
+let lastOps;
+let lastOpsIndex;
+let lastSlice;
+let firstSlice;
+let evLastSlice;
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if (Boolean(evalLine.innerText) == false) {
+        k = String(currentExpression);
+        if (displayLine.innerHTML == "" && evalLine.innerHTML == "") {
             currentExpression += button.textContent;
             updateBox();
             clearLine();
         }
-        else {
-            currentExpression = "";
-            currentExpression += button.textContent;
-            updateBox();
-            clearLine();
+        else if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+                currentExpression = (displayLine.innerHTML) + button.textContent;
+                updateBox();
+                updateDsp();
+                clearLine();
         }
-    });
+        else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
+                currentExpression = button.value;
+                updateBox();
+                updateDsp();
+                clearLine();
+            }
+        }
+    )
 });
 
-// 00 button 
+// single & double zero buttons 
 doubleZero.addEventListener('click', () => {
-    if (currentExpression == "" && currentDisplay == "") {
+    if (displayLine.innerHTML == "" && evalLine.innerHTML == "") {
         clearLine();
         clearBox();
         resetExp();
     }
-    else if (currentExpression != "" && currentDisplay == "") {
-        currentExpression += "00";
-        updateBox();
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+      k = String(currentExpression);
+
+
+        if (k.indexOf("+") < 1 & k.indexOf("-") < 1 & k.indexOf("/") < 1 & k.indexOf("*") < 1) {
+            currentExpression += "00";
+            updateBox();
+        }
+        else {
+            if (k.charAt(k.length - 1) == addOps | k.charAt(k.length - 1) == subOps | k.charAt(k.length - 1) == mulOps | k.charAt(k.length - 1) == divOps) {
+                currentExpression += "";
+                updateBox();
+            }
+            else {
+                currentExpression += "00";
+                updateBox();
+            }
+        }
     }
-    else if (currentExpression != "" && currentDisplay != "") {
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
         currentExpression = 0;
         updateBox();
         clearLine();
-        resetExp();
-        resetDsp();
     }
 });
-// zero
+
+
 numberZero.addEventListener('click', () => {
-    if (currentExpression == "" && currentDisplay == "") {
+    if (displayLine.innerHTML == "" && evalLine.innerHTML == "") {
+        currentExpression = '';
         clearLine();
-        clearBox();
-        resetExp();
-    }
-    else if (currentExpression != "" && currentDisplay == "") {
-        currentExpression += "0";
         updateBox();
     }
-    else if (currentExpression != "" && currentDisplay != "") {
-        currentExpression = 0;
+    else if (displayLine.innerHTML == "0" && evalLine.innerHTML == "") {
+        currentExpression += "";
+        updateBox();
+    }
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+     k = String(currentExpression);
+
+
+        if (k.indexOf("+") < 1 & k.indexOf("-") < 1 & k.indexOf("/") < 1 & k.indexOf("*") < 1) {
+            currentExpression += "0";
+            updateBox();
+        }
+        else {
+            if (k.charAt(k.length - 1) == addOps | k.charAt(k.length - 1) == subOps | k.charAt(k.length - 1) == mulOps | k.charAt(k.length - 1) == divOps) {
+                currentExpression += "";
+                updateBox();
+            }
+            else if (k.charAt(k.length - 1) == "0") {
+                currentExpression += "0";
+                updateBox();
+            }
+            else {
+                currentExpression += "0";
+                updateBox();
+            }
+        }
+    }
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
+        currentExpression = '';
         updateBox();
         clearLine();
         resetExp();
         resetDsp();
     }
 });
-// float point
+
+//  float
 pointBtn.addEventListener('click', () => {
-    (currentExpression == 0) ? pointF1() : pointF2();
-    function pointF1() {
-        currentExpression = "0.";
-        displayLine.innerHTML = "0.";
+    k = String(currentExpression);
+    if (displayLine.innerHTML == "" && evalLine.innerHTML == "") {
+        currentExpression = "0."
         updateBox();
     }
-    function pointF2() {
-        currentExpression += `${memory}`;
-        updateBox();
-        clearLine();
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+        if (k.charAt(k.length - 1) == '.') {
+            currentExpression += "";
+            updateBox();
+        }
+        else if ((k.charAt(k.length - 1) == addOps) || (k.charAt(k.length - 1) == subOps) || (k.charAt(k.length - 1) == mulOps) || (k.charAt(k.length - 1) == divOps)) {
+            currentExpression += "0.";
+            updateBox();
+        }
+        else if ((k.charAt(k.length - 1) == '0') || (k.charAt(k.length - 1) == '1') || (k.charAt(k.length - 1) == '2') || (k.charAt(k.length - 1) == '3') || (k.charAt(k.length - 1) == '4') || (k.charAt(k.length - 1) == '5') || (k.charAt(k.length - 1) == '6') || (k.charAt(k.length - 1) == '7') || (k.charAt(k.length - 1) == '8') || (k.charAt(k.length - 1) == '9')) {
+            if ((k.includes(addOps) == true) || (k.includes(divOps) == true) || (k.includes(subOps) == true) || (k.includes(mulOps) == true)) {
+                lastIndex = Math.max(k.lastIndexOf(addOps), k.lastIndexOf(subOps), k.lastIndexOf(mulOps), k.lastIndexOf(divOps));
+                lastOps = k.charAt(lastIndex);
+                lastOpsIndex = k.lastIndexOf(lastOps);
+                lastSlice = k.slice(lastOpsIndex, k.length)
+                if (lastSlice.includes('.') == true) {
+                    currentExpression += '';
+                    updateBox();
+                }
+                else {
+                    currentExpression += '.';
+                    updateBox();
+                }
+            }
+            else {
+                firstSlice = k.slice(0);
+                if (firstSlice.includes('.') == true) {
+                    currentExpression += '';
+                    updateBox();
+                }
+                else {
+                    currentExpression += '.';
+                    updateBox();
+                }
+            }
+        }
+    }
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
+        memory = evalLine.textContent;
+        if (memory.includes('.') == false) {
+            currentExpression =String(memory) + '.';
+           updateBox();
+            clearLine();
+        }
+        else if (memory.includes('.') == true) {
+            currentExpression ="0.";
+            updateBox();
+            clearLine();
+            resetDsp();
+            resetExp();
+            
+        }
     }
 });
+
 
 // operators
 opsBtn.forEach(button => {
     button.addEventListener('click', () => {
-        if (currentExpression == "") {
+        k = String(currentExpression)
+        if (displayLine.innerHTML == "" && evalLine.innerHTML == "") {
             currentExpression = 0 + button.value;
             updateBox();
             clearLine();
         }
-        else {
-            currentExpression += button.value;
-            updateBox();
+        else if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+            if ((k.charAt(k.length - 1) == ".") || (k.charAt(k.length - 1) == addOps) || (k.charAt(k.length - 1) == subOps) || (k.charAt(k.length - 1) == mulOps) || (k.charAt(k.length - 1) == divOps)) {
+                currentExpression += "";
+                updateBox();
+            }
+            else {
+                
+                currentExpression += button.value;
+                updateBox();
+                clearLine();
+            }
+        }
+        else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
+            resetExp();
             clearLine();
+            currentExpression = (currentDisplay * 1) + button.value;
+            updateBox();
         }
     });
 });
@@ -109,14 +235,25 @@ opsBtn.forEach(button => {
 
 // equal
 evBtn.addEventListener('click', () => {
-    if (Boolean(displayLine.innerHTML) == true && Boolean(evalLine.innerHTML) == false) {
-        let a = eval(currentExpression);
-        currentExpression = a.toString();
-        updateDsp();
-        updateLine();
-
+    if (displayLine.innerHTML != "" && evalLine.innerHTML == "") {
+        k = String(currentExpression);
+        lastIndex = Math.max(k.lastIndexOf(addOps), k.lastIndexOf(subOps), k.lastIndexOf(mulOps), k.lastIndexOf(divOps));
+        lastOps = k.charAt(lastIndex);
+        lastOpsIndex = k.lastIndexOf(lastOps);
+        evLastSlice = k.slice(0, lastOpsIndex)
+        if ((k.charAt(k.length - 1) == addOps) || (k.charAt(k.length - 1) == subOps) || (k.charAt(k.length - 1) == mulOps) || k.charAt(k.length - 1) == divOps) {
+            currentDisplay = eval(evLastSlice);
+            updateLine();
+            displayLine.innerHTML = currentDisplay;
+        }
+        else {
+            let render = eval(currentExpression);
+            currentExpression = render;
+            updateDsp();
+            updateLine();
+        };
     }
-    else if (Boolean(displayLine.innerHTML) == true && Boolean(evalLine.innerHTML) == true) {
+    else if (displayLine.innerHTML != "" && evalLine.innerHTML != "") {
         resetExp();
         clearLine();
         clearBox();
@@ -160,8 +297,9 @@ function updateDsp() {
 function resetDsp() {
     currentDisplay = "";
 };
-
-
 function resetExp() {
     currentExpression = "";
 }
+
+
+

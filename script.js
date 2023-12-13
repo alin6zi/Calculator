ee3a3d0935b8d35c8ab20770d13c06f78307a60d
AliNazi <125884879+alin6zi@users.noreply.github.com>
2023-12-13 22:41:02 +0330

    update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ numberZero.addEventListener('click', () => {
         currentExpression = '';
         clearLine();
         updateBox();
-        resetDsp();
+        resetExp();
     }
     else if (displayLine.innerHTML == "0" && evalLine.innerHTML == "") {
         currentExpression += "";
